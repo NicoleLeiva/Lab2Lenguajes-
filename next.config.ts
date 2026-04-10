314adf760a07826ef100dd302f9cc11831630f7d
Melissa <melissaleivaf@gmail.com>
2026-04-09 19:37:40 -0600

    ajuste a package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+module.exports = {
+  allowedDevOrigins: ['10.243.238.240'],
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
 
 export default nextConfig;
